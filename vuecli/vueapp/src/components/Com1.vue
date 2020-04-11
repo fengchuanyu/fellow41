042f9013378e748138d77041eb5a01fa557ba00f
fengchuanyu <fengchuanyu@163.com>
2020-04-11 11:53:36 +0800

    admin

---
+++ vuecli/vueapp/src/components/Com1.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>组件一</h1>
+    <h2>子组件参数:{{str}}</h2>
+    <button @click="changeVal">change</button>
+  </div>
+</template>
+<script>
+export default {
+  props:["str"],
+  methods: {
+    changeVal(){
+      this.$emit("update:str","你好");
+    }
+  },
+}
+</script>
--- vuecli/vueapp/src/views/About.vue
+++ vuecli/vueapp/src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <com1 v-bind:str.sync="a"></com1>
+    <h2>父组件参数：{{a}}</h2>
   </div>
 </template>
+<script>
+import Com1 from "../components/Com1.vue"
+export default {
+  data() {
+    return {
+      a:"hello fellow41"
+    }
+  },
+  components:{
+    Com1
+  }
+}
+</script>

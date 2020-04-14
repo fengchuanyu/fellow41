33d6c52fe468f961f2e4c1990b581bd0eca893e3
fengchuanyu <fengchuanyu@163.com>
2020-04-14 10:08:56 +0800

    admin

--- vuecli/myapp/src/App.vue
+++ vuecli/myapp/src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <test-com></test-com>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>|
+      <router-link to="/tv">Tv</router-link>
+    </div>
+    <router-view/>
+    
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TestCom from '@/components/TestCom.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    TestCom
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin:0;
+  padding:0;
+}
+li{
+  list-style: none;
 }
 </style>
---
+++ vuecli/myapp/src/components/TestCom.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    测试组件{{str}}
+    <button @click="addHandle">add</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      str:"hello"
+    }
+  },
+  methods: {
+    addHandle(){
+      console.log("123")
+    }
+  },
+}
+</script>
+<style scoped>
+  
+</style>

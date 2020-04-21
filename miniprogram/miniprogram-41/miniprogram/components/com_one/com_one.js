9325dee60eb15f1e7af9e66d0f003a04f759a09a
fengchuanyu <fengchuanyu@163.com>
2020-04-21 14:54:08 +0800

    admin

---
+++ miniprogram/miniprogram-41/miniprogram/components/com_one/com_one.js
@@ -0,0 +1,30 @@
+// components/com_one/com_one.js
+Component({
+  options: {
+    styleIsolation: 'shared',
+    multipleSlots: true // 在组件定义时的选项中启用多slot支持
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    str:{
+      type:String,
+      value:"你好"
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
